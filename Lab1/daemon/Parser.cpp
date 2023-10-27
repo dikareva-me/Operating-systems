198f614032f58943082b4bee8c4a4aa54dc5fc3a
dikareva-me <mary1voight@gmail.com>
2023-10-27 11:53:46 +0300

    parsing config file

---
+++ Lab1/daemon/Parser.cpp
@@ -0,0 +1,50 @@
+#include <sys/syslog.h>
+#include <iostream>
+#include "Parser.hpp"
+
+Parser Parser::_instance;
+
+Parser &Parser::getInstance() {
+    return _instance;
+}
+
+void Parser::parseConfig(std::string &configPath) {
+    std::ifstream configFile(configPath);
+
+    if(!_config.empty()) {
+        _config.clear();
+    }
+    if (!configFile.is_open()) {
+        throw std::runtime_error("Can't open config file");
+    }
+
+    for(int i =0; i< _grammar.getGrammarCount(); i++){
+        std::string parameter;
+        std::string value;
+        configFile >> parameter;
+        configFile >> value;
+        Grammar::ConfigParams cp = _grammar.getGrammarValue(parameter);
+        
+        if(cp == Grammar::DEFAULT) {
+            throw std::runtime_error("Can't open config file");
+        }
+        _config.insert({cp, value});
+    }
+    syslog(LOG_INFO, "DONE");
+}
+
+
+Parser::Grammar::ConfigParams Parser::Grammar::getGrammarValue(const std::string& value) {
+    Parser::Grammar::ConfigParams result = _grammar.at(value);
+    return result;
+}
+
+Parser::Grammar::Grammar():
+    _grammar({
+        {"time", ConfigParams::TIME_DELAY},
+        {"input_dir", ConfigParams::DIRECTORY1},
+        {"output_dir", ConfigParams::DIRECTORY2} }) {};
+
+int Parser::Grammar::getGrammarCount() {
+    return _grammar.size();
+}
